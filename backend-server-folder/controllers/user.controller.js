bfdcee66bc1015fae3ef24fc151abaa8e3ca03ca
Lucas Henry <henrylucas755@gmail.com>
2024-06-03 21:00:23 -0400

    endpoint frame

--- backend-server-folder/controllers/user.controller.js
+++ backend-server-folder/controllers/user.controller.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+
+//ENDPOINT: Create new user
+
+//ENDPOINT: Login
+
+//ENDPOINT: Edit user
+
+//ENDPOINT: Get user by tag <--- WIP
+
+//ENDPOINT: Delete user
 
 module.exports = router;
