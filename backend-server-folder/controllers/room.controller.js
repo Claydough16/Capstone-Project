4a2cb73a00c8bcb427266d523b87f466cb3eb874
Lucas Henry <henrylucas755@gmail.com>
2024-06-05 18:04:28 -0400

    uhhhh

---
+++ backend-server-folder/controllers/room.controller.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const Room = require("../models/room.model");
+const validateSession = require("../middleware/validate.session");
+
+//ENDPOINT: Create new room
+router.post("/", async (req, res) => {
+  try {
+    const { name, description, addedUsers } = req.body;
+    const room = new Room({ name, description, addedUsers });
+
+    const newRoom = await room.save();
+    res.status(200).json({
+      result: newRoom,
+      message: `${newRoom.name} added to colllection`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      Error: err.message,
+    });
+  }
+});
+
+//ENDPOINT: Get all rooms. Does not check user auth to view rooms.
+// >>We may want to implement this server side. Right now it would be filtered client side
+router.get("/all", async (req, res) => {
+  try {
+    const getAllRooms = await Room.find();
+    res.status(200).json({
+      result: getAllRooms,
+    });
+  } catch (err) {
+    res.status(500).json({
+      Error: err.message,
+    });
+  }
+});
+
+module.exports = router;
