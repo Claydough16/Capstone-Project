a3c9faf7820c6d5c58b21cb566edd1bc7193c402
Clayton Nicholson <claynicholson199@gmail.com>
2024-07-02 21:43:44 -0500

    Reverted email.js to only utilize nodemailer and not send grid

--- backend-server-folder/email.js
+++ backend-server-folder/email.js
@@ -1,18 +1,22 @@
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
-const User = require('./models/users.model');
-require('dotenv').config();
+const User = require("./models/users.model");
 
 const sendPasswordResetMail = async (email) => {
     try {
+        // Generate SMTP service account from ethereal.email
+        let account = await nodemailer.createTestAccount();
+
         console.log('Credentials obtained, sending message...');
 
-        // Create a SMTP transporter object using SendGrid
+        // Create a SMTP transporter object
         let transporter = nodemailer.createTransport({
-            service: 'SendGrid',
+            host: account.smtp.host,
+            port: account.smtp.port,
+            secure: account.smtp.secure,
             auth: {
-                user: 'apikey', // this is fixed value
-                pass: process.env.SENDGRID_API_KEY // your SendGrid API key
+                user: account.user,
+                pass: account.pass
             }
         });
 
@@ -31,24 +35,24 @@ const sendPasswordResetMail = async (email) => {
 
         // Message object
         let message = {
-            from: `Sender Name <${process.env.EMAIL}>`,
-            to: `Recipient <${email}>`,
+            from: 'Sender Name <sender@example.com>',
+            to: `Recipient <${ email }>`,
             subject: 'Use the link below to reset your password',
             text: 'Hello to myself!',
-            html: `<a href="http://localhost:3000/password-reset?email=${email}&token=${resetToken}">Reset password</a>`
+            html: `<a href=http://localhost:3000/password-reset?email=${email}&token=${resetToken}>Reset password</a>`
         };
 
-        // Send email
-        let info = await transporter.sendMail(message);
+    // Send email
+    let info = await transporter.sendMail(message);
 
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        return nodemailer.getTestMessageUrl(info);
-    } catch (err) {
-        console.error('Error sending email:', err.message);
-        throw err;
-    }
+    return nodemailer.getTestMessageUrl(info);
+} catch (err) {
+    console.error('Error sending email:', err.message);
+    throw err;
+}
 }
 
-module.exports = sendPasswordResetMail;
+module.exports = sendPasswordResetMail
